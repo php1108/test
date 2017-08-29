841bc022e40d800c0b6d4efeadc58302de1a69e3
周文刚 <php1108>
2017-08-29 18:56:08 +0800

    no message

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'laravel');
+
+// Project repository
+set('repository', 'https://github.com/php1108/test.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('php128.top')
+    ->user('www-deploy')
+    ->set('deploy_path', '/var/www/{{application}}');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+//before('deploy:symlink', 'artisan:migrate');
+
